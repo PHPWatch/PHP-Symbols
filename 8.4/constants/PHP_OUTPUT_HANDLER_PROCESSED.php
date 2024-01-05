5e8c48079ffbcbaf369d63c9e0abcb92b50a5363
Ayesh <Ayesh@users.noreply.github.com>
2024-01-05 05:30:48 +0000

    Update database

---
+++ 8.4/constants/PHP_OUTPUT_HANDLER_PROCESSED.php
@@ -0,0 +1,28 @@
+<?php
+
+return array (
+  'type' => 'constant',
+  'name' => 'PHP_OUTPUT_HANDLER_PROCESSED',
+  'meta' => 
+  array (
+    'type' => 'constant',
+    'name' => 'PHP_OUTPUT_HANDLER_PROCESSED',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'added' => '0.0',
+    'deprecated' => NULL,
+    'removed' => NULL,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'PHP_OUTPUT_HANDLER_PROCESSED constant (php.net)',
+        'url' => 'https://www.php.net/manual/reserved.constants.php#constant.php-output-handler-processed',
+      ),
+    ),
+  ),
+  'value' => 16384,
+  'extension' => 'Core',
+);
