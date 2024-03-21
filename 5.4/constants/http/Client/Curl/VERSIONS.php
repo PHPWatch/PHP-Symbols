95119097c39217fdeb1c60c3120684e45f547319
Ayesh <Ayesh@users.noreply.github.com>
2024-03-21 04:31:47 +0000

    2024 Mar 21

--- 5.4/constants/http/Client/Curl/VERSIONS.php
+++ 5.4/constants/http/Client/Curl/VERSIONS.php
@@ -18,6 +18,6 @@ return array (
     array (
     ),
   ),
-  'value' => 'libcurl/7.81.0 GnuTLS/3.7.3 zlib/1.2.11 brotli/1.0.9 zstd/1.4.8 libidn2/2.3.2 libpsl/0.21.0 (+libidn2/2.3.2) libssh/0.9.6/openssl/zlib nghttp2/1.43.0 librtmp/2.3 OpenLDAP/2.5.16',
+  'value' => 'libcurl/7.81.0 GnuTLS/3.7.3 zlib/1.2.11 brotli/1.0.9 zstd/1.4.8 libidn2/2.3.2 libpsl/0.21.0 (+libidn2/2.3.2) libssh/0.9.6/openssl/zlib nghttp2/1.43.0 librtmp/2.3 OpenLDAP/2.5.17',
   'extension' => 'http',
 );
