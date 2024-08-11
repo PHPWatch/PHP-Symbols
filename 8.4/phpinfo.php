a5077852d65ea12662a6fda0ad7df01316a5459f
Ayesh <Ayesh@users.noreply.github.com>
2024-08-11 12:52:52 +0000

    2024 Aug 11

--- 8.4/phpinfo.php
+++ 8.4/phpinfo.php
@@ -734,7 +734,7 @@ Max keys => 16229
 OOM restarts => 0
 Hash keys restarts => 0
 Manual restarts => 0
-Start time => 2024-08-11T01:59:26+0000
+Start time => 2024-08-11T12:51:57+0000
 Last restart time => none
 Last force restart time => none
 
