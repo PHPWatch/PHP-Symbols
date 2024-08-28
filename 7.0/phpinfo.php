cf3985185772edbf1cd9329faae0be1a85bc0c2e
Ayesh <Ayesh@users.noreply.github.com>
2024-08-28 15:40:58 +0000

    2024 Aug 28

--- 7.0/phpinfo.php
+++ 7.0/phpinfo.php
@@ -268,9 +268,9 @@ enchant
 enchant support => enabled
 Version => 7.0.33
 
-hunspell => Hunspell Provider => /usr/lib/x86_64-linux-gnu/enchant-2/enchant_hunspell.so
 hspell => Hspell Provider => /usr/lib/x86_64-linux-gnu/enchant-2/enchant_hspell.so
 aspell => Aspell Provider => /usr/lib/x86_64-linux-gnu/enchant-2/enchant_aspell.so
+hunspell => Hunspell Provider => /usr/lib/x86_64-linux-gnu/enchant-2/enchant_hunspell.so
 
 exif
 
