765792b8e6d263585ab65899d199f42e5ddff8d0
Ayesh <Ayesh@users.noreply.github.com>
2024-11-19 05:47:44 +0000

    2024 Nov 19

--- 8.5/const.php
+++ 8.5/const.php
@@ -32,7 +32,7 @@ return array (
     'PHP_RELEASE_VERSION' => 0,
     'PHP_EXTRA_VERSION' => '-dev',
     'PHP_VERSION_ID' => 80500,
-    'PHP_BUILD_DATE' => 'Nov 19 2024 05:39:24',
+    'PHP_BUILD_DATE' => 'Nov 19 2024 05:42:38',
     'PHP_ZTS' => false,
     'PHP_DEBUG' => false,
     'PHP_OS' => 'Linux',
