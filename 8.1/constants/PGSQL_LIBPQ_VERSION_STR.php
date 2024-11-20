5de2ca8001634e6798bcccc5933ccda4f3d28a6c
Ayesh <Ayesh@users.noreply.github.com>
2024-11-20 02:12:21 +0000

    2024 Nov 20

--- 8.1/constants/PGSQL_LIBPQ_VERSION_STR.php
+++ 8.1/constants/PGSQL_LIBPQ_VERSION_STR.php
@@ -19,6 +19,6 @@ return array (
     array (
     ),
   ),
-  'value' => '17.1',
+  'value' => '17.0',
   'extension' => 'pgsql',
 );
--- 8.2/const.php
+++ 8.2/const.php
@@ -2534,8 +2534,8 @@ return array (
   ),
   'pgsql' => 
   array (
-    'PGSQL_LIBPQ_VERSION' => '17.1',
-    'PGSQL_LIBPQ_VERSION_STR' => '17.1',
+    'PGSQL_LIBPQ_VERSION' => '17.0',
+    'PGSQL_LIBPQ_VERSION_STR' => '17.0',
     'PGSQL_CONNECT_FORCE_NEW' => 2,
     'PGSQL_CONNECT_ASYNC' => 4,
     'PGSQL_ASSOC' => 1,
