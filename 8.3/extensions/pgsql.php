e7b74c4f07006879f616624185a16a40ec26bb33
Ayesh <Ayesh@users.noreply.github.com>
2024-11-20 15:45:09 +0000

    2024 Nov 20

--- 8.3/extensions/pgsql.php
+++ 8.3/extensions/pgsql.php
@@ -153,8 +153,8 @@ return array (
   ),
   'constants' => 
   array (
-    'PGSQL_LIBPQ_VERSION' => '17.0',
-    'PGSQL_LIBPQ_VERSION_STR' => '17.0',
+    'PGSQL_LIBPQ_VERSION' => '17.1',
+    'PGSQL_LIBPQ_VERSION_STR' => '17.1',
     'PGSQL_CONNECT_FORCE_NEW' => 2,
     'PGSQL_CONNECT_ASYNC' => 4,
     'PGSQL_ASSOC' => 1,
