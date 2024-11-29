3f2574a9b4b01f97e0158c0519dfd501d2ab29d9
Ayesh <Ayesh@users.noreply.github.com>
2024-11-29 15:45:53 +0000

    2024 Nov 29 - 12087365888
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/12087365888

---
+++ 8.0/functions/ocibindbyname.php
@@ -0,0 +1,46 @@
+<?php
+
+return array (
+  'type' => 'function',
+  'name' => 'ocibindbyname',
+  'meta' => 
+  array (
+    'type' => 'function',
+    'name' => 'ocibindbyname',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'deprecated' => true,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'ocibindbyname function (php.net)',
+        'url' => 'https://www.php.net/manual/function.ocibindbyname.php',
+      ),
+    ),
+  ),
+  'doc' => false,
+  'parameters' => 
+  array (
+  ),
+  'return' => 
+  array (
+    'type' => 'ReflectionNamedType',
+    'nullable' => false,
+  ),
+  'extension' => 'oci8',
+  'toString' => 'Function [ <internal, deprecated:oci8> function ocibindbyname ] {
+
+  - Parameters [5] {
+    Parameter #0 [ <required> $statement ]
+    Parameter #1 [ <required> string $param ]
+    Parameter #2 [ <required> mixed &$var ]
+    Parameter #3 [ <optional> int $max_length = -1 ]
+    Parameter #4 [ <optional> int $type = 0 ]
+  }
+  - Return [ bool ]
+}
+',
+);
--- 8.0/phpinfo.php
+++ 8.0/phpinfo.php
@@ -407,6 +407,29 @@ Tracing => n/a
 Loaded plugins => mysqlnd,debug_trace,auth_plugin_mysql_native_password,auth_plugin_mysql_clear_password,auth_plugin_caching_sha2_password,auth_plugin_sha256_password
 API Extensions => mysqli,pdo_mysql
 
+oci8
+
+OCI8 Support => enabled
+OCI8 DTrace Support => disabled
+OCI8 Version => 3.0.1
+Oracle Run-time Client Library Version => 23.6.0.24.10
+Oracle Compile-time Instant Client Version => 23.6
+
+Directive => Local Value => Master Value
+oci8.connection_class => no value => no value
+oci8.default_prefetch => 100 => 100
+oci8.events => Off => Off
+oci8.max_persistent => -1 => -1
+oci8.old_oci_close_semantics => Off => Off
+oci8.persistent_timeout => -1 => -1
+oci8.ping_interval => 60 => 60
+oci8.privileged_connect => Off => Off
+oci8.statement_cache_size => 20 => 20
+
+Statistics =>  
+Active Persistent Connections => 0
+Active Connections => 0
+
 odbc
 
 ODBC Support => enabled
@@ -461,7 +484,7 @@ pcre.recursion_limit => 100000 => 100000
 PDO
 
 PDO support => enabled
-PDO drivers => dblib, firebird, mysql, odbc, pgsql, sqlite
+PDO drivers => dblib, firebird, mysql, oci, odbc, pgsql, sqlite
 
 pdo_dblib
 
@@ -480,6 +503,10 @@ Client API version => mysqlnd 8.0.30
 
 Directive => Local Value => Master Value
 pdo_mysql.default_socket => /var/run/mysqld/mysqld.sock => /var/run/mysqld/mysqld.sock
+
+PDO_OCI
+
+PDO Driver for OCI 8 and later => enabled
 
 PDO_ODBC
 
