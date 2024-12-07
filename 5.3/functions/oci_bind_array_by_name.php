f293e14f8e96a8e4b0ebbeefe271b5ef0b63d4df
Ayesh <Ayesh@users.noreply.github.com>
2024-12-07 20:43:00 +0000

    2024 Dec 07 - 12215899497
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/12215899497

---
+++ 5.3/functions/oci_bind_array_by_name.php
@@ -0,0 +1,42 @@
+<?php
+
+return array (
+  'type' => 'function',
+  'name' => 'oci_bind_array_by_name',
+  'meta' => 
+  array (
+    'type' => 'function',
+    'name' => 'oci_bind_array_by_name',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'deprecated' => false,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'oci_bind_array_by_name function (php.net)',
+        'url' => 'https://www.php.net/manual/function.oci-bind-array-by-name.php',
+      ),
+    ),
+  ),
+  'doc' => false,
+  'parameters' => 
+  array (
+  ),
+  'return' => NULL,
+  'extension' => 'oci8',
+  'toString' => 'Function [ <internal:oci8> function oci_bind_array_by_name ] {
+
+  - Parameters [6] {
+    Parameter #0 [ <required> $statement_resource ]
+    Parameter #1 [ <required> $column_name ]
+    Parameter #2 [ <required> &$variable ]
+    Parameter #3 [ <required> $maximum_array_length ]
+    Parameter #4 [ <optional> $maximum_item_length ]
+    Parameter #5 [ <optional> $type ]
+  }
+}
+',
+);
---
+++ 5.3/functions/oci_result.php
@@ -0,0 +1,38 @@
+<?php
+
+return array (
+  'type' => 'function',
+  'name' => 'oci_result',
+  'meta' => 
+  array (
+    'type' => 'function',
+    'name' => 'oci_result',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'deprecated' => false,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'oci_result function (php.net)',
+        'url' => 'https://www.php.net/manual/function.oci-result.php',
+      ),
+    ),
+  ),
+  'doc' => false,
+  'parameters' => 
+  array (
+  ),
+  'return' => NULL,
+  'extension' => 'oci8',
+  'toString' => 'Function [ <internal:oci8> function oci_result ] {
+
+  - Parameters [2] {
+    Parameter #0 [ <required> $statement_resource ]
+    Parameter #1 [ <required> $column_number_or_name ]
+  }
+}
+',
+);
