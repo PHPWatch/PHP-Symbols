153e9751fe724245f492322494254fb61635e952
Ayesh <811553+Ayesh@users.noreply.github.com>
2025-01-21 15:42:10 +0000

    2025 Jan 21 - 12890311410
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/12890311410

---
+++ 7.0/functions/ocisavelob.php
@@ -0,0 +1,39 @@
+<?php
+
+return array (
+  'type' => 'function',
+  'name' => 'ocisavelob',
+  'meta' => 
+  array (
+    'type' => 'function',
+    'name' => 'ocisavelob',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'deprecated' => false,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'ocisavelob function (php.net)',
+        'url' => 'https://www.php.net/manual/function.ocisavelob.php',
+      ),
+    ),
+  ),
+  'doc' => false,
+  'parameters' => 
+  array (
+  ),
+  'return' => NULL,
+  'extension' => 'oci8',
+  'toString' => 'Function [ <internal:oci8> function ocisavelob ] {
+
+  - Parameters [3] {
+    Parameter #0 [ <required> $lob_descriptor ]
+    Parameter #1 [ <required> $data ]
+    Parameter #2 [ <optional> $offset ]
+  }
+}
+',
+);
