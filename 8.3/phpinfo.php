fa49d0a5f2384d23061a49ad0d04df9304ff439e
Ayesh <811553+Ayesh@users.noreply.github.com>
2025-01-22 20:40:39 +0000

    2025 Jan 22 - 12916544780
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/12916544780

--- 8.3/phpinfo.php
+++ 8.3/phpinfo.php
@@ -280,8 +280,8 @@ enchant support => enabled
 Libenchant Version => 2.3.3
 
 aspell => Aspell Provider => /usr/lib/x86_64-linux-gnu/enchant-2/enchant_aspell.so
-hspell => Hspell Provider => /usr/lib/x86_64-linux-gnu/enchant-2/enchant_hspell.so
 hunspell => Hunspell Provider => /usr/lib/x86_64-linux-gnu/enchant-2/enchant_hunspell.so
+hspell => Hspell Provider => /usr/lib/x86_64-linux-gnu/enchant-2/enchant_hspell.so
 
 exif
 
