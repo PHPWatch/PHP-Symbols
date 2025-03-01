93a868629577ef38bf75f873d1a3d5cbdb0f195f
Ayesh <811553+Ayesh@users.noreply.github.com>
2025-03-01 05:41:11 +0000

    2025 Mar 01 - 13601566917
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/13601566917

---
+++ 7.0/functions/oci_lob_flush.php
@@ -0,0 +1,38 @@
+<?php
+
+return array (
+  'type' => 'function',
+  'name' => 'oci_lob_flush',
+  'meta' => 
+  array (
+    'type' => 'function',
+    'name' => 'oci_lob_flush',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'deprecated' => false,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'oci_lob_flush function (php.net)',
+        'url' => 'https://www.php.net/manual/function.oci-lob-flush.php',
+      ),
+    ),
+  ),
+  'doc' => false,
+  'parameters' => 
+  array (
+  ),
+  'return' => NULL,
+  'extension' => 'oci8',
+  'toString' => 'Function [ <internal:oci8> function oci_lob_flush ] {
+
+  - Parameters [2] {
+    Parameter #0 [ <required> $lob_descriptor ]
+    Parameter #1 [ <optional> $flag ]
+  }
+}
+',
+);
---
+++ 7.0/functions/ociexecute.php
@@ -0,0 +1,38 @@
+<?php
+
+return array (
+  'type' => 'function',
+  'name' => 'ociexecute',
+  'meta' => 
+  array (
+    'type' => 'function',
+    'name' => 'ociexecute',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'deprecated' => false,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'ociexecute function (php.net)',
+        'url' => 'https://www.php.net/manual/function.ociexecute.php',
+      ),
+    ),
+  ),
+  'doc' => false,
+  'parameters' => 
+  array (
+  ),
+  'return' => NULL,
+  'extension' => 'oci8',
+  'toString' => 'Function [ <internal:oci8> function ociexecute ] {
+
+  - Parameters [2] {
+    Parameter #0 [ <required> $statement_resource ]
+    Parameter #1 [ <optional> $mode ]
+  }
+}
+',
+);
