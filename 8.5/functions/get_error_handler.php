b5fdee1dbb72be2856563a2e5effe5228f6e0bf1
Ayesh <811553+Ayesh@users.noreply.github.com>
2025-03-27 15:42:45 +0000

    2025 Mar 27 - 14110557994
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/14110557994

---
+++ 8.5/functions/get_error_handler.php
@@ -0,0 +1,41 @@
+<?php
+
+return array (
+  'type' => 'function',
+  'name' => 'get_error_handler',
+  'meta' => 
+  array (
+    'type' => 'function',
+    'name' => 'get_error_handler',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'deprecated' => false,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'get_error_handler function (php.net)',
+        'url' => 'https://www.php.net/manual/function.get-error-handler.php',
+      ),
+    ),
+  ),
+  'doc' => false,
+  'parameters' => 
+  array (
+  ),
+  'return' => 
+  array (
+    'type' => 'ReflectionNamedType',
+    'nullable' => true,
+  ),
+  'extension' => 'Core',
+  'toString' => 'Function [ <internal:Core> function get_error_handler ] {
+
+  - Parameters [0] {
+  }
+  - Return [ ?callable ]
+}
+',
+);
---
+++ 8.5/functions/get_exception_handler.php
@@ -0,0 +1,41 @@
+<?php
+
+return array (
+  'type' => 'function',
+  'name' => 'get_exception_handler',
+  'meta' => 
+  array (
+    'type' => 'function',
+    'name' => 'get_exception_handler',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'deprecated' => false,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'get_exception_handler function (php.net)',
+        'url' => 'https://www.php.net/manual/function.get-exception-handler.php',
+      ),
+    ),
+  ),
+  'doc' => false,
+  'parameters' => 
+  array (
+  ),
+  'return' => 
+  array (
+    'type' => 'ReflectionNamedType',
+    'nullable' => true,
+  ),
+  'extension' => 'Core',
+  'toString' => 'Function [ <internal:Core> function get_exception_handler ] {
+
+  - Parameters [0] {
+  }
+  - Return [ ?callable ]
+}
+',
+);
