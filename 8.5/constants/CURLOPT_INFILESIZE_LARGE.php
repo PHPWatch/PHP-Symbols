0dacb70a07d3289f2c7344caa951788b119ffb03
Ayesh <811553+Ayesh@users.noreply.github.com>
2025-04-09 09:30:31 +0000

    2025 Apr 09 - 14353566891
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/14353566891

---
+++ 8.5/constants/CURLOPT_INFILESIZE_LARGE.php
@@ -0,0 +1,29 @@
+<?php
+
+return array (
+  'type' => 'constant',
+  'name' => 'CURLOPT_INFILESIZE_LARGE',
+  'meta' => 
+  array (
+    'type' => 'constant',
+    'name' => 'CURLOPT_INFILESIZE_LARGE',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'added' => '0.0',
+    'deprecated' => false,
+    'deprecated_message' => NULL,
+    'removed' => NULL,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'CURLOPT_INFILESIZE_LARGE constant (php.net)',
+        'url' => 'https://www.php.net/manual/curl.constants.php#constant.curlopt-infilesize-large',
+      ),
+    ),
+  ),
+  'value' => 30115,
+  'extension' => 'curl',
+);
