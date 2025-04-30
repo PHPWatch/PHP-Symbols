f1c8d4e88e0af7f8d922c361b81c8634f5fefc42
Ayesh <811553+Ayesh@users.noreply.github.com>
2025-04-30 02:23:14 +0000

    2025 Apr 30 - 14745228729
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/14745228729

---
+++ 8.5/constants/CURLFOLLOW_OBEYCODE.php
@@ -0,0 +1,29 @@
+<?php
+
+return array (
+  'type' => 'constant',
+  'name' => 'CURLFOLLOW_OBEYCODE',
+  'meta' => 
+  array (
+    'type' => 'constant',
+    'name' => 'CURLFOLLOW_OBEYCODE',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'added' => '0.0',
+    'deprecated' => false,
+    'deprecated_message' => NULL,
+    'removed' => NULL,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'CURLFOLLOW_OBEYCODE constant (php.net)',
+        'url' => 'https://www.php.net/manual/curl.constants.php#constant.curlfollow-obeycode',
+      ),
+    ),
+  ),
+  'value' => 2,
+  'extension' => 'curl',
+);
