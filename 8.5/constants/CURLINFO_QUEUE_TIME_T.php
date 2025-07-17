f2893a668b48435eeac07ac732a488051f65f543
Ayesh <811553+Ayesh@users.noreply.github.com>
2025-07-17 15:48:59 +0000

    2025 Jul 17 - 16349524397
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/16349524397

---
+++ 8.5/constants/CURLINFO_QUEUE_TIME_T.php
@@ -0,0 +1,29 @@
+<?php
+
+return array (
+  'type' => 'constant',
+  'name' => 'CURLINFO_QUEUE_TIME_T',
+  'meta' => 
+  array (
+    'type' => 'constant',
+    'name' => 'CURLINFO_QUEUE_TIME_T',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'added' => '0.0',
+    'deprecated' => false,
+    'deprecated_message' => NULL,
+    'removed' => NULL,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'CURLINFO_QUEUE_TIME_T constant (php.net)',
+        'url' => 'https://www.php.net/manual/curl.constants.php#constant.curlinfo-queue-time-t',
+      ),
+    ),
+  ),
+  'value' => 6291521,
+  'extension' => 'curl',
+);
