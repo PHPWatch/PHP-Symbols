bd364456e6db85a5b007494c1a89d034cb241052
Ayesh <811553+Ayesh@users.noreply.github.com>
2025-07-29 05:56:59 +0000

    2025 Jul 29 - 16587755980
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/16587755980

---
+++ 7.2/constants/OCI_DTYPE_FILE.php
@@ -0,0 +1,24 @@
+<?php
+
+return array (
+  'type' => 'constant',
+  'name' => 'OCI_DTYPE_FILE',
+  'meta' => 
+  array (
+    'type' => 'constant',
+    'name' => 'OCI_DTYPE_FILE',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'added' => '0.0',
+    'deprecated' => false,
+    'deprecated_message' => NULL,
+    'removed' => NULL,
+    'resources' => 
+    array (
+    ),
+  ),
+  'value' => 56,
+  'extension' => 'oci8',
+);
---
+++ 7.2/constants/OCI_FO_RETRY.php
@@ -0,0 +1,24 @@
+<?php
+
+return array (
+  'type' => 'constant',
+  'name' => 'OCI_FO_RETRY',
+  'meta' => 
+  array (
+    'type' => 'constant',
+    'name' => 'OCI_FO_RETRY',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'added' => '0.0',
+    'deprecated' => false,
+    'deprecated_message' => NULL,
+    'removed' => NULL,
+    'resources' => 
+    array (
+    ),
+  ),
+  'value' => 25410,
+  'extension' => 'oci8',
+);
