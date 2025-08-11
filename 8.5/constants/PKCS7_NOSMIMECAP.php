957dbb4e478f08a091b18f05eac045804d881c90
Ayesh <811553+Ayesh@users.noreply.github.com>
2025-08-11 20:43:47 +0000

    2025 Aug 11 - 16891431099
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/16891431099

---
+++ 8.5/constants/PKCS7_NOSMIMECAP.php
@@ -0,0 +1,24 @@
+<?php
+
+return array (
+  'type' => 'constant',
+  'name' => 'PKCS7_NOSMIMECAP',
+  'meta' => 
+  array (
+    'type' => 'constant',
+    'name' => 'PKCS7_NOSMIMECAP',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'added' => '0.0',
+    'deprecated' => false,
+    'deprecated_message' => NULL,
+    'removed' => NULL,
+    'resources' => 
+    array (
+    ),
+  ),
+  'value' => 512,
+  'extension' => 'openssl',
+);
---
+++ 8.5/constants/PKCS7_NO_DUAL_CONTENT.php
@@ -0,0 +1,24 @@
+<?php
+
+return array (
+  'type' => 'constant',
+  'name' => 'PKCS7_NO_DUAL_CONTENT',
+  'meta' => 
+  array (
+    'type' => 'constant',
+    'name' => 'PKCS7_NO_DUAL_CONTENT',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'added' => '0.0',
+    'deprecated' => false,
+    'deprecated_message' => NULL,
+    'removed' => NULL,
+    'resources' => 
+    array (
+    ),
+  ),
+  'value' => 65536,
+  'extension' => 'openssl',
+);
