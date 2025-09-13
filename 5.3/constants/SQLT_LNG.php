b88383163b64843f75d353355de0bb9955d5e7a4
Ayesh <811553+Ayesh@users.noreply.github.com>
2025-09-13 10:42:44 +0000

    2025 Sep 13 - 17695340718
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/17695340718

---
+++ 5.3/constants/SQLT_LNG.php
@@ -0,0 +1,24 @@
+<?php
+
+return array (
+  'type' => 'constant',
+  'name' => 'SQLT_LNG',
+  'meta' => 
+  array (
+    'type' => 'constant',
+    'name' => 'SQLT_LNG',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'added' => '0.0',
+    'deprecated' => false,
+    'deprecated_message' => NULL,
+    'removed' => NULL,
+    'resources' => 
+    array (
+    ),
+  ),
+  'value' => 8,
+  'extension' => 'oci8',
+);
---
+++ 5.3/functions/ocicollsize.php
@@ -0,0 +1,37 @@
+<?php
+
+return array (
+  'type' => 'function',
+  'name' => 'ocicollsize',
+  'meta' => 
+  array (
+    'type' => 'function',
+    'name' => 'ocicollsize',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'deprecated' => false,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'ocicollsize function (php.net)',
+        'url' => 'https://www.php.net/manual/function.ocicollsize.php',
+      ),
+    ),
+  ),
+  'doc' => false,
+  'parameters' => 
+  array (
+  ),
+  'return' => NULL,
+  'extension' => 'oci8',
+  'toString' => 'Function [ <internal:oci8> function ocicollsize ] {
+
+  - Parameters [1] {
+    Parameter #0 [ <required> $collection ]
+  }
+}
+',
+);
