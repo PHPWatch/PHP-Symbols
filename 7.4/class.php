8566e6beef0a978b74d6c195b06ba951ac2da933
Ayesh <811553+Ayesh@users.noreply.github.com>
2025-09-20 15:40:17 +0000

    2025 Sep 20 - 17881622290
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/17881622290

--- 7.4/class.php
+++ 7.4/class.php
@@ -164,6 +164,8 @@ return array (
   'mysqli_warning',
   'mysqli_result',
   'mysqli_stmt',
+  'OCI-Lob',
+  'OCI-Collection',
   'PharException',
   'Phar',
   'PharData',
---
+++ 7.4/functions/oci_lob_write.php
@@ -0,0 +1,39 @@
+<?php
+
+return array (
+  'type' => 'function',
+  'name' => 'oci_lob_write',
+  'meta' => 
+  array (
+    'type' => 'function',
+    'name' => 'oci_lob_write',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'deprecated' => false,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'oci_lob_write function (php.net)',
+        'url' => 'https://www.php.net/manual/function.oci-lob-write.php',
+      ),
+    ),
+  ),
+  'doc' => false,
+  'parameters' => 
+  array (
+  ),
+  'return' => NULL,
+  'extension' => 'oci8',
+  'toString' => 'Function [ <internal:oci8> function oci_lob_write ] {
+
+  - Parameters [3] {
+    Parameter #0 [ <required> $lob_descriptor ]
+    Parameter #1 [ <required> $string ]
+    Parameter #2 [ <optional> $length ]
+  }
+}
+',
+);
