dd90029f2e6a72b365cca72c147e3d941dc4735d
Ayesh <811553+Ayesh@users.noreply.github.com>
2026-02-01 03:48:30 +0000

    2026 Feb 01 - 21555981508
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/21555981508

--- 8.6/functions/array_filter.php
+++ 8.6/functions/array_filter.php
@@ -36,7 +36,7 @@ return array (
   - Parameters [3] {
     Parameter #0 [ <required> array $array ]
     Parameter #1 [ <optional> ?callable $callback = null ]
-    Parameter #2 [ <optional> int $mode = 0 ]
+    Parameter #2 [ <optional> int $mode = ARRAY_FILTER_USE_KEY ]
   }
   - Return [ array ]
 }
