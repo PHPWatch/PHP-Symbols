58f418d31b944fcca6103c5c1666cf5b0ff6d35a
Ayesh <811553+Ayesh@users.noreply.github.com>
2026-02-19 20:58:21 +0000

    2026 Feb 19 - 22199562846
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/22199562846

--- 8.0/phpinfo.php
+++ 8.0/phpinfo.php
@@ -516,7 +516,7 @@ ODBC Connection Pooling => Enabled, strict matching
 pdo_pgsql
 
 PDO Driver for PostgreSQL => enabled
-PostgreSQL(libpq) Version => 18.2
+PostgreSQL(libpq) Version => 18.1
 
 pdo_sqlite
 
@@ -526,7 +526,7 @@ SQLite Library => 3.45.1
 pgsql
 
 PostgreSQL Support => enabled
-PostgreSQL (libpq) Version => 18.2
+PostgreSQL (libpq) Version => 18.1
 Multibyte character support => enabled
 Active Persistent Links => 0
 Active Links => 0
