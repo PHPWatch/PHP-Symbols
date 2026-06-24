82e7827a33c18f0991e4605b3fc55b653a79cf11
Ayesh <811553+Ayesh@users.noreply.github.com>
2026-06-24 02:47:12 +0000

    2026 Jun 24 - 28071339748
    Action run: https://github.com/PHPWatch/symbol-data-builder/actions/runs/28071339748

---
+++ 8.6/constants/CURLINFO_SIZE_DELIVERED.php
@@ -0,0 +1,29 @@
+<?php
+
+return array (
+  'type' => 'constant',
+  'name' => 'CURLINFO_SIZE_DELIVERED',
+  'meta' => 
+  array (
+    'type' => 'constant',
+    'name' => 'CURLINFO_SIZE_DELIVERED',
+    'description' => '',
+    'keywords' => 
+    array (
+    ),
+    'added' => '0.0',
+    'deprecated' => false,
+    'deprecated_message' => NULL,
+    'removed' => NULL,
+    'resources' => 
+    array (
+      0 => 
+      array (
+        'name' => 'CURLINFO_SIZE_DELIVERED constant (php.net)',
+        'url' => 'https://www.php.net/manual/curl.constants.php#constant.curlinfo-size-delivered',
+      ),
+    ),
+  ),
+  'value' => 6291527,
+  'extension' => 'curl',
+);
